eef9895d5f2997186e56279097cc641c25fb6506
Jose Daniel <jshdniel@gmail.com>
2023-03-18 17:34:54 -0500

    Recomendaciones aplicadas 03

--- js-1/sum.js
+++ js-1/sum.js
@@ -1,5 +1,6 @@
 
 function sum(number) {
+    if (number <= 0) return 0;
     let total = 0;
     for(let i = 1; i <= number; i++) {
         total += i;
@@ -8,6 +9,6 @@ function sum(number) {
     return total;
 }
 
-console.log(sum(4))
-console.log(sum(10))
-console.log(sum(15))
+console.log(sum(4));
+console.log(sum(10));
+console.log(sum(15));
